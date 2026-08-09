c308a05b447b9a7ea1302797897be02d73aa2269
Victor Lustosa <victor.lustosacp@gmail.com>
2026-08-09 16:38:44 -0300

    doc: dinâmica

---
+++ dia01_variaveis.py
@@ -0,0 +1,7 @@
+# rotudo que guarda algum dado.
+#Não precisa declarar o tipo da variável
+# Isso significa que o Python é uma linguagem de tipagem dinâmica.
+nome = "Marina";
+idade = 28;
+altura = 1.66;
+trabalhando = True;
