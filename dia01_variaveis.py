92e2820b38fa7b3d0f228352d219cd53ed1c2913
Victor Lustosa <victor.lustosacp@gmail.com>
2026-08-09 16:44:01 -0300

    doc: basico

--- dia01_variaveis.py
+++ dia01_variaveis.py
@@ -5,3 +5,19 @@ nome = "Marina";
 idade = 28;
 altura = 1.66;
 trabalhando = True;
+
+#typeof em python é type()
+
+print(type(nome));
+print(type(altura));
+
+# Podemos reatribuir valores
+
+print(nome);
+nome = "Marina Pena";
+print(nome);
+
+# Juntar texto com variaveis
+
+print(f"O nome da minha namorada é {nome} e ela tem {idade} anos. Hoje é -> 16/08/2026 ");
+
